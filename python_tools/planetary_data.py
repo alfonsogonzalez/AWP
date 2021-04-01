dcd6bfe6cee544790b5920d806e9020746f512be
alfonso <alfonso8gonzalez@gmail.com>
2021-03-31 18:24:15 -0700

    Add initial version of planetary_data.py

---
+++ python_tools/planetary_data.py
@@ -0,0 +1,23 @@
+'''
+AWP
+Planetary data file
+'''
+
+# gravitational constant
+G_meters = 6.67430e-11       # m**3/kg/s**2
+G        = G_meters * 10**-9 # km**3/kg/s**2
+
+earth = {
+		'name'            : 'Earth',
+		'spice_name'      : 'EARTH',
+		'SPICE_ID'        : 399,
+		'mass'            : 5.972e24,
+		'mu'              : 5.972e24 * G,
+		'radius'          : 6378.0,
+		'J2'              : 1.081874e-3,
+		'sma'             : 149.596e6, # km
+		'SOI'             : 926006.6608, # km
+		'deorbit_altitude': 100.0, # km
+		'cmap'            : 'Blues',
+		'body_fixed_frame': 'ITRF93'
+		}
