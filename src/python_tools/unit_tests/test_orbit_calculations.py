3784f0dda1112f02ce515d0e8a8184db4a10ff41
alfonso <alfonso8gonzalez@gmail.com>
2021-07-20 05:00:22 -0700

    Add two_body_ode_ones unit test

--- src/python_tools/unit_tests/test_orbit_calculations.py
+++ src/python_tools/unit_tests/test_orbit_calculations.py
@@ -19,3 +19,8 @@ pytestmark = pytest.mark.filterwarnings( 'error' )
 def test_two_body_ode_zero_division_expect_throw():
 	with pytest.raises( RuntimeWarning ):
 		oc.two_body_ode( 0.0, np.zeros( 6 ) )
+
+def test_two_body_ode_ones():
+	a = oc.two_body_ode( 0.0,
+		np.array( [ 1.0, 0, 0, 0, 0, 0 ] ), mu = 1.0 )
+	assert np.all( a == np.array( [ 0, 0, 0, -1.0, 0, 0 ] ) )
