131753f97b41d79757cf2b08d4791c36a83b219c
alfonso <alfonso8gonzalez@gmail.com>
2021-07-22 20:11:58 -0700

    Add frame_transform_same_frame unit test

--- src/python_tools/unit_tests/test_numerical_tools.py
+++ src/python_tools/unit_tests/test_numerical_tools.py
@@ -25,3 +25,10 @@ def test_norm_basic_usage():
 def test_normed_zero_division_expect_throw():
 	with pytest.raises( RuntimeWarning ):
 		nt.normed( np.zeros( 3 ) )
+
+def test_frame_transform_same_frame():
+	arr = np.array( [ [ 1.0, 0.0, 0.0 ], [ 1.0, 0.0, 0.0] ] )
+	arr_transformed = nt.frame_transform(
+		arr, [ 0.0, 0.0 ], 'J2000', 'J2000' )
+	assert np.all( arr[ 0, : ] == arr_transformed[ 0, : ] )
+	assert np.all( arr[ 1, : ] == arr_transformed[ 1, : ] )
