8e1c4e1e7300ce51164354dba3989d2b85855d1d
alfonso <alfonso8gonzalez@gmail.com>
2021-08-28 09:16:17 -0700

    Remove dependency on AWP environment variable to locate paths

--- src/python_tools/spice_data.py
+++ src/python_tools/spice_data.py
@@ -18,7 +18,6 @@ directory
 Here is an example of how to set this environment variable
 from the terminal (I use bash):
 $ export AWP=/home/alfonso/pub/AWP/
-'''
 
 AWP_path = os.environ.get( 'AWP' )
 
@@ -26,7 +25,11 @@ if AWP_path is not None:
 	base_dir = AWP_path
 else:
 	base_dir = '..'
+'''
+base_dir = os.path.join(
+	os.path.dirname( os.path.realpath( __file__ ) ),
+	'../../data/spice' )
 
-leapseconds_kernel = os.path.join( base_dir, 'data/spice/lsk/naif0012.tls' )
-de432              = os.path.join( base_dir, 'data/spice/spk/de432s.bsp'   )
-pck00010           = os.path.join( base_dir, 'data/spice/pck/pck00010.tpc' )
+leapseconds_kernel = os.path.join( base_dir, 'lsk/naif0012.tls' )
+de432              = os.path.join( base_dir, 'spk/de432s.bsp'   )
+pck00010           = os.path.join( base_dir, 'pck/pck00010.tpc' )
