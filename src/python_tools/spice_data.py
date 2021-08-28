ea65aae11345ea87fb50a6b1c1cf006b10bbd50f
alfonso <alfonso8gonzalez@gmail.com>
2021-08-28 09:58:11 -0700

    Use os.path.join for paths to remove operating system distinctions

--- src/python_tools/spice_data.py
+++ src/python_tools/spice_data.py
@@ -8,27 +8,10 @@ SPICE data filepaths
 
 import os
 
-'''
-If you set an environment variable named AWP to be
-your local path to the base of this repository, you will
-be able to run all scripts from any directory that you're
-currently in. If not, you must run everything from the src/
-directory
-
-Here is an example of how to set this environment variable
-from the terminal (I use bash):
-$ export AWP=/home/alfonso/pub/AWP/
-
-AWP_path = os.environ.get( 'AWP' )
-
-if AWP_path is not None:
-	base_dir = AWP_path
-else:
-	base_dir = '..'
-'''
 base_dir = os.path.join(
 	os.path.dirname( os.path.realpath( __file__ ) ),
-	'../../data/spice' )
+	os.path.join( '..', '..', 'data', 'spice' ) 
+	)
 
 leapseconds_kernel = os.path.join( base_dir, 'lsk/naif0012.tls' )
 de432              = os.path.join( base_dir, 'spk/de432s.bsp'   )
