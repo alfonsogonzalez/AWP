edc8ae1a5b76b39150a164630a2e9f0d22b53c88
alfonso <alfonso8gonzalez@gmail.com>
2022-04-28 15:32:50 -0700

    Add default_configs.js

---
+++ docs/default_configs.js
@@ -0,0 +1,64 @@
+/*
+AID | Astrodynamics Interactive Demo with HTML/CSS/JavaScript, Alfonso Gonzalez
+https://github.com/alfonsogonzalez/AWP
+https://www.youtube.com/c/AlfonsoGonzalezSpaceEngineering
+
+Default configs script
+*/
+
+const DEFAULT_CONFIGS = [
+{
+    "planet": "earth",
+    "rx0": "7000",
+    "ry0": "0",
+    "rz0": "0",
+    "vx0": "0",
+    "vy0": "8.6037",
+    "vz0": "0",
+    "dt0": "30",
+    "sim-time0": "2",
+    "active0": false,
+    "rx1": "7000",
+    "ry1": "0",
+    "rz1": "0",
+    "vx1": "0",
+    "vy1": "8.6037",
+    "vz1": "0",
+    "dt1": "30",
+    "sim-time1": "1",
+    "active1": true,
+    "sma0": "10000",
+    "ecc0": "0.3",
+    "inc0": "0",
+    "ta0": "0",
+    "aop0": "0",
+    "raan0": "90",
+    "dt-k0": "30",
+    "sim-time-k0": "1",
+    "active-k0": true,
+    "sma1": "10000",
+    "ecc1": "0.3",
+    "inc1": "60",
+    "ta1": "0",
+    "aop1": "0",
+    "raan1": "90",
+    "dt-k1": "30",
+    "sim-time-k1": "1",
+    "active-k1": true,
+    "sma2": "10000",
+    "ecc2": "0.3",
+    "inc2": "60",
+    "ta2": "0",
+    "aop2": "180",
+    "raan2": "90",
+    "dt-k2": "30",
+    "sim-time-k2": "1",
+    "active-k2": true,
+    "hs-checkbox": true,
+    "vinf-checkbox": false,
+    "vinf-x": 1,
+    "vinf-y": 2,
+    "vinf-z": 3
+}
+];
+
